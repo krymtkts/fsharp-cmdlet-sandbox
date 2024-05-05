7b6c2b222dc6943590c44c835779a38bc961488e
krymtkts <takatoshi.kuriyama@gmail.com>
2024-05-05 20:04:12 +0900

    Add a test module.

---
+++ src/wrap-dus-or-not/Library.fs
@@ -0,0 +1,24 @@
+﻿namespace wrap_dus_or_not
+
+open System.Collections
+open System.Management.Automation
+
+[<Cmdlet(VerbsData.Out, "ObjectWrappedDUs")>]
+[<OutputType(typeof<PSObject>)>]
+type OutObjectWrappedDUsCommand() =
+    inherit PSCmdlet()
+
+    let mutable input: obj Generic.List = Generic.List()
+
+    [<Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)>]
+    member val InputObject: PSObject [] = [||] with get, set
+
+    override __.BeginProcessing() = base.BeginProcessing()
+
+    override __.ProcessRecord() =
+        for io in __.InputObject do
+            input.Add(io)
+
+    override __.EndProcessing() =
+        // print memory usage.
+        printfn "Memory usage: %d" (System.GC.GetTotalMemory(true))
---
+++ test-wrap-dus-or-not.ps1
@@ -0,0 +1,3 @@
+dotnet clean
+dotnet publish
+Import-Module .\src\wrap-dus-or-not\bin\Debug\*\publish\*.psd1
