eb4711cfdc9219889fd8448b09f6365ae414b88f
krymtkts <takatoshi.kuriyama@gmail.com>
2024-05-06 10:14:51 +0900

    Add test modes for the PSObject memory usage testing.

--- src/wrap-dus-or-not/Library.fs
+++ src/wrap-dus-or-not/Library.fs
@@ -3,22 +3,65 @@
 open System.Collections
 open System.Management.Automation
 
+[<RequireQualifiedAccess>]
+[<NoComparison>]
+type Entry =
+    | Obj of PSObject
+    | Dict of DictionaryEntry
+
 [<Cmdlet(VerbsData.Out, "ObjectWrappedDUs")>]
 [<OutputType(typeof<PSObject>)>]
 type OutObjectWrappedDUsCommand() =
     inherit PSCmdlet()
 
-    let mutable input: obj Generic.List = Generic.List()
+    let input: obj Generic.List = Generic.List()
+    let props: obj Generic.HashSet = Generic.HashSet()
+
+    let add (io: PSObject) =
+        match io.BaseObject with
+        | :? IDictionary as dct ->
+            for d in Seq.cast<DictionaryEntry> dct do
+                d |> Entry.Dict |> input.Add
+        | _ -> io |> Entry.Obj |> input.Add
+
+    let addProps (io: PSObject) =
+        match io.BaseObject with
+        | :? IDictionary as dct ->
+            for d in Seq.cast<DictionaryEntry> dct do
+                d |> PSObject.AsPSObject |> _.Properties |> Seq.iter (fun p -> p |> _.Name |> props.Add |> ignore)
+        | _ ->
+            for p in io.Properties do
+                p |> _.Name |> props.Add |> ignore
 
     [<Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)>]
     member val InputObject: PSObject [] = [||] with get, set
+
+    [<Parameter(Mandatory = true)>]
+    [<ValidateSet("Raw", "DUs", "Properties", "Both")>]
+    [<ValidateNotNullOrEmpty>]
+    member val Mode: string = null with get, set
 
     override __.BeginProcessing() = base.BeginProcessing()
 
     override __.ProcessRecord() =
         for io in __.InputObject do
-            input.Add(io)
+            match __.Mode with
+            | "Raw" ->
+                io |> input.Add
+            | "DUs" ->
+                io |> add
+            | "Properties" ->
+                io |> addProps
+            | "Both" ->
+                io |> add
+                io |> addProps
+            | _ -> ()
 
     override __.EndProcessing() =
         // print memory usage.
         printfn "Memory usage: %d" (System.GC.GetTotalMemory(true))
+
+// Raw          259,237,176
+// DUs          283,093,752
+// Properties    56,019,104
+// Both       1,922,577,960
