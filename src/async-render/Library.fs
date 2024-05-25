51e09f453a4a484ea618da60f8968650a91344f6
krymtkts <takatoshi.kuriyama@gmail.com>
2024-05-25 20:55:11 +0900

    Add a empty cmdlet for asynchronous rendering.

---
+++ src/async-render/Library.fs
@@ -0,0 +1,24 @@
+﻿namespace async_render
+
+open System.Collections
+open System.Management.Automation
+
+[<RequireQualifiedAccess>]
+[<NoComparison>]
+type Entry =
+    | Obj of PSObject
+    | Dict of DictionaryEntry
+
+[<Cmdlet(VerbsData.Out, "GridAsync")>]
+[<OutputType(typeof<PSObject>)>]
+type OutGridAsyncCommand() =
+    inherit PSCmdlet()
+
+    [<Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)>]
+    member val InputObject: PSObject [] = [||] with get, set
+
+    override __.BeginProcessing() = ()
+
+    override __.ProcessRecord() = ()
+
+    override __.EndProcessing() = ()
