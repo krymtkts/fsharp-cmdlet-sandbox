f9ef8aceaa6a4168d2f8640bd7701f214ff3836f
krymtkts <takatoshi.kuriyama@gmail.com>
2024-07-20 17:32:18 +0900

    Add a empty cmdlet.

---
+++ src/fast-search/Library.fs
@@ -0,0 +1,26 @@
+﻿namespace ``fast-search``
+
+open System
+open System.Management.Automation
+open System.Management.Automation.Runspaces
+
+[<Cmdlet(VerbsCommon.Find, "ObjectWithPattern")>]
+[<OutputType(typeof<PSObject>)>]
+type FindObjectWithPatternCommand() =
+    inherit PSCmdlet()
+
+    [<Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)>]
+    member val InputObject: PSObject [] = [||] with get, set
+
+    override __.BeginProcessing() =
+        printfn "==========Begin processing"
+
+        printfn "==========Begin processing done"
+
+    override __.ProcessRecord() =
+        printfn "==========Add %A" __.InputObject
+
+    override __.EndProcessing() =
+        printfn "==========End processing"
+
+        printfn "==========End processing done"
