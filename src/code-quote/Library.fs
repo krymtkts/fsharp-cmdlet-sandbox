7d31e8736d4e681446883a4b0b07e30a18fc6a02
krymtkts <takatoshi.kuriyama@gmail.com>
2024-09-14 13:58:34 +0900

    Add a empty cmdlet project for testing.

---
+++ src/code-quote/Library.fs
@@ -0,0 +1,26 @@
+﻿namespace code_quote
+
+open System.Collections
+open System.Management.Automation
+
+[<RequireQualifiedAccess>]
+[<NoComparison>]
+type Entry =
+    | Obj of PSObject
+    | Dict of DictionaryEntry
+
+[<Cmdlet(VerbsCommon.Select, "ObjectTest")>]
+[<OutputType(typeof<PSObject>)>]
+type SelectObjectTestCommand() =
+    inherit PSCmdlet()
+
+    [<Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)>]
+    member val InputObject: PSObject [] = [||] with get, set
+
+    override __.BeginProcessing() = ()
+
+    override __.ProcessRecord() =
+        for io in __.InputObject do
+            io |> __.WriteObject
+
+    override __.EndProcessing() = ()
