918db7aaad8c413e2efb9168395393fcfe10efc8
krymtkts <takatoshi.kuriyama@gmail.com>
2024-09-16 16:50:15 +0900

    Add notes.

--- test-code-quote.ps1
+++ test-code-quote.ps1
@@ -15,7 +15,9 @@ Import-Module .\src\code-quote\bin\Release\*\publish\*.psd1 -Force
             Size = $Size
         }
         Measure-Command { $raw | Select-ObjectTest @Params -Mode Lambda }
+        # NOTE: Compiling Code Quotations takes more than 100ms, but generated expressions are generally faster.
         Measure-Command { $raw | Select-ObjectTest @Params -Mode CodeQuotation }
+        # NOTE: Invoke consistency checks secondly to avoid suppressing the effect of the first run.
         $raw | Select-ObjectTest @Params -Mode Lambda | Measure-Object
         $raw | Select-ObjectTest @Params -Mode CodeQuotation | Measure-Object
     }
