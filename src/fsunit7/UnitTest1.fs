c4c8197cba539b70aad95539fb33a34be3e0072f
krymtkts <takatoshi.kuriyama@gmail.com>
2024-12-27 17:32:38 +0900

    Update fsunit7 project for testing.

--- src/fsunit7/UnitTest1.fs
+++ src/fsunit7/UnitTest1.fs
@@ -1,7 +1,10 @@
 module UnitTest1
 
 open Xunit
+// open FsUnitTyped
 
 [<Fact>]
-let Test1() =
-    Assert.True(true)
+let Test1 () = Assert.True(true)
+
+// [<Fact>]
+// let Test2 () = true |> shouldEqual true
