6faf69d96e8fea73989e6144f487dfcf75270b6d
krymtkts <takatoshi.kuriyama@gmail.com>
2025-06-10 21:27:59 +0900

    Naive implementation for testing Avalonia.FuncUI.

--- src/avalonia-funcui/Library.fs
+++ src/avalonia-funcui/Library.fs
@@ -1,6 +1,80 @@
-﻿namespace prerelease_test_module
+﻿namespace avalonia_funcui
 
 open System.Management.Automation
+
+open Avalonia
+open Avalonia.Controls
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Elmish
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.FuncUI.DSL
+open Elmish
+
+module Main =
+    type State = { message: string }
+
+    let init () = { message = "Hello, World!" }, Cmd.none
+
+    type Msg =
+        | Send of string
+        | NoOp
+
+    let update (msg: Msg) (state: State) : State * Cmd<_> =
+        match msg with
+        | Send newMessage -> { state with message = newMessage }, Cmd.none
+        | _ -> state, Cmd.none
+
+    let view (state: State) (dispatch: Msg -> unit) =
+        DockPanel.create [
+
+                           DockPanel.children [
+
+                                                TextBlock.create [
+
+                                                                   TextBlock.text state.message
+                                                                   DockPanel.dock Dock.Top
+
+                                                                    ]
+
+                                                Button.create [
+
+                                                                Button.content "Click Me"
+                                                                Button.onClick (fun _ ->
+                                                                    dispatch (Send "Button Clicked!"))
+                                                                DockPanel.dock Dock.Bottom
+
+                                                                 ]
+
+                                                 ]
+
+                            ]
+
+type MainWindow() as this =
+    inherit HostWindow()
+
+    do
+        base.Title <- "Example"
+        base.Height <- 300.0
+        base.Width <- 300.0
+
+        Elmish.Program.mkProgram Main.init Main.update Main.view
+        |> Program.withHost this
+        |> Program.run
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            let mainWindow = MainWindow()
+            desktopLifetime.MainWindow <- mainWindow
+        | _ -> ()
 
 [<Cmdlet(VerbsDiagnostic.Test, "AvaloniaFuncUI")>]
 [<OutputType(typeof<PSObject>)>]
@@ -11,4 +85,10 @@ type SelectPocofCommand() =
 
     override __.ProcessRecord() = printfn "Hello from AvaloniaFuncUI"
 
-    override __.EndProcessing() = base.EndProcessing()
+    override __.EndProcessing() =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(Array.empty)
+        |> ignore
--- test-avalonia-funcui.ps1
+++ test-avalonia-funcui.ps1
@@ -2,6 +2,10 @@
 param (
 )
 
+# dotnet add ./src/avalonia-funcui package Avalonia.Desktop
+# dotnet add ./src/avalonia-funcui package Avalonia.Themes.Fluent
+# dotnet add ./src/avalonia-funcui package Avalonia.FuncUI
+# dotnet add ./src/avalonia-funcui package Avalonia.FuncUI.Elmish
 dotnet publish .\src\avalonia-funcui
 Import-Module .\src\avalonia-funcui\bin\Release\*\publish\*.psd1 -Force
 Test-AvaloniaFuncUI
